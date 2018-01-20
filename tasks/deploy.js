49a2353431e96859708b713c61525f089b45b45f
Erwin Verdonk <e.verdonk@gmail.com>
2018-01-20 10:27:27 +0100

    Update deploy script and package.json to let the custom resource be required by other applications

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,39 +1,44 @@
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'eu-west-1' });
+exports.default = (() => {
+  const AWS = require('aws-sdk');
+  AWS.config.update({ region: 'eu-west-1' });
 
-const fs = require('fs');
-const cf = new AWS.CloudFormation();
-const cfTemplate = fs.readFileSync('cloudformation.yaml');
-const code = fs.readFileSync('./src/index.js');
+  const fs = require('fs');
+  const cf = new AWS.CloudFormation();
+  const cfTemplate = fs.readFileSync(__dirname + '/../cloudformation.yaml');
+  const code = fs.readFileSync(__dirname + '/../src/index.js');
 
-cf.createStack({
-  StackName: 'CFNCustomResource-S3EmptyBucket',
-  TemplateBody: cfTemplate.toString(),
-  Capabilities: ['CAPABILITY_IAM'],
-  Parameters: [
-    {
-      ParameterKey: 'Code',
-      ParameterValue: code.toString(),
-    },
-  ],
-})
-.promise()
-.catch(_ => {
-  if (_.code === 'AlreadyExistsException') {
-    cf.updateStack({
-      StackName: 'CFNCustomResource-S3EmptyBucket',
-      TemplateBody: cfTemplate.toString(),
-      Capabilities: ['CAPABILITY_IAM'],
-      Parameters: [
-        {
-          ParameterKey: 'Code',
-          ParameterValue: code.toString(),
-        },
-      ],
-    })
-    .promise()
-    .catch(_ => console.error(_));
-  } else {
-    console.error(_);
-  }
-});
+  return cf.createStack({
+    StackName: 'CFNCustomResource-S3EmptyBucket',
+    TemplateBody: cfTemplate.toString(),
+    Capabilities: ['CAPABILITY_IAM'],
+    Parameters: [
+      {
+        ParameterKey: 'Code',
+        ParameterValue: code.toString(),
+      },
+    ],
+  })
+  .promise()
+  .catch(_ => {
+    if (_.code === 'AlreadyExistsException') {
+      cf.updateStack({
+        StackName: 'CFNCustomResource-S3EmptyBucket',
+        TemplateBody: cfTemplate.toString(),
+        Capabilities: ['CAPABILITY_IAM'],
+        Parameters: [
+          {
+            ParameterKey: 'Code',
+            ParameterValue: code.toString(),
+          },
+        ],
+      })
+      .promise()
+      .catch(_ => _.message.indexOf(('no updates').toLowerCase()) > -1
+        ? console.error(_)
+        : _
+      );
+    } else {
+      console.error(_);
+    }
+  });
+})();
