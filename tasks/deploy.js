9cb0cbf2cd3c516b73eef4ddbc83b81aea6e24dc
Erwin Verdonk <erwin@instantmagazine.com>
2018-02-12 12:23:44 +0100

    Add extra logging + a waitFor stack creation/update to complete

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -7,6 +7,7 @@ const cf = new AWS.CloudFormation();
 const cfTemplate = fs.readFileSync(__dirname + '/../lib/cloudformation.yaml');
 const code = fs.readFileSync(__dirname + '/../lib/S3EmptyBucket.js');
 
+console.log(`Creation of CloudFormation stack '${stackName}' started`);
 exports.default = cf.createStack({
   StackName: 'CFNCustomResource-S3EmptyBucket',
   TemplateBody: cfTemplate.toString(),
@@ -19,6 +20,8 @@ exports.default = cf.createStack({
   ],
 })
 .promise()
+.then(_ => cf.waitFor('stackCreateComplete', { StackName: stackName }).promise())
+.then(_ => console.log(`Creation of CloudFormation stack '${stackName}' finished`))
 .catch(_ => {
   if (_.code === 'AlreadyExistsException') {
     cf.updateStack({
@@ -33,6 +36,8 @@ exports.default = cf.createStack({
       ],
     })
     .promise()
+    .then(_ => cf.waitFor('stackUpdateComplete', { StackName: stackName }).promise())
+    .then(_ => console.log(`Update of CloudFormation stack '${stackName}' finished`))
     .catch(_ => _.message.indexOf(('no updates').toLowerCase()) > -1
       ? console.error(_)
       : _
